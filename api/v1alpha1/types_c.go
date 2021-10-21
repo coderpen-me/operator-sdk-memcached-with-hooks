54042568c396d281713772160b9e7fb358c99a7c
Adarsh Pandey <coderpen@gmail.com>
2021-10-21 15:07:58 +0530

    --

--- api/v1alpha1/types_c.go
+++ api/v1alpha1/types_c.go
@@ -1,12 +1,13 @@
 package v1alpha1
 
 type Plugin1 struct {
-	Field1 Field1         `json:"field1"`
+	Field1 struct {
+		Field4 int `json:"field4"`
+		Field5 struct {
+			Field6 int `json:"field6"`
+			Field7 int `json:"field7"`
+		} `json:"field5"`
+	} `json:"field1"`
 	Field2 map[string]int `json:"field2"`
 	Field3 map[string]int `json:"field3"`
-}
-
-type Field1 struct {
-	Field4 map[string]int `json:"field4"`
-	Field5 map[string]int `json:"field5"`
 }
